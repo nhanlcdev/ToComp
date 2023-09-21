19ae055969fdfd0e8bcfbb62fa0e91591b062248
nhanlc <nhanlc@vng.com.vn>
2023-09-21 14:33:56 +0700

    feat(ui): init task component

--- app/src/main/java/com/example/tocomp/MainActivity.kt
+++ app/src/main/java/com/example/tocomp/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.tocomp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.tocomp.ui.theme.ToCompTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +28,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val sampleTask by remember {
+                        mutableStateOf(TaskData("Title", false, "Description"))
+                    }
 
+                    Task(sampleTask) { sampleTask.isDone = it; Log.d("Task", "isDone: $it") }
                 }
             }
+        }
+    }
+}
+
+class TaskData(
+    title: String,
+    isDone: Boolean,
+    description: String = ""
+){
+    var title by mutableStateOf(title)
+    var isDone by mutableStateOf(isDone)
+    var description by mutableStateOf(description)
+}
+
+@Composable
+fun Task(task: TaskData, onCheckedChange: (Boolean) -> Unit){
+    Row {
+        Checkbox(checked = task.isDone, onCheckedChange = onCheckedChange)
+        Column {
+            Text(text = task.title)
+            Text(text = task.description)
         }
     }
 }
