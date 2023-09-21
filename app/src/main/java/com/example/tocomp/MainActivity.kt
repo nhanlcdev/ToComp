005aa23b9df9598d0fdf2ec1d2488521407fd80d
nhanlc <nhanlc@vng.com.vn>
2023-09-21 14:40:32 +0700

    feat(task): enable editting title & description

--- app/src/main/java/com/example/tocomp/MainActivity.kt
+++ app/src/main/java/com/example/tocomp/MainActivity.kt
@@ -1,16 +1,18 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.tocomp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,13 +34,20 @@ class MainActivity : ComponentActivity() {
                         mutableStateOf(TaskData("Title", false, "Description"))
                     }
 
-                    Task(sampleTask) { sampleTask.isDone = it; Log.d("Task", "isDone: $it") }
+                    Task(sampleTask)
                 }
             }
         }
     }
 }
 
+/**
+ * A data class that represents a task.
+ *
+ * @param title The title of the task
+ * @param isDone Whether the task is done or not
+ * @param description The description of the task
+ */
 class TaskData(
     title: String,
     isDone: Boolean,
@@ -49,13 +58,29 @@ class TaskData(
     var description by mutableStateOf(description)
 }
 
+/**
+ * A task composable that displays a checkbox, title and description.
+ *
+ * @param task The task to display
+ * @param onTitleChange Called when the title is changed.
+ *  Default implementation updates the task title
+ * @param onDescriptionChange Called when the description is changed.
+ *  Default implementation updates the task description
+ * @param onCheckedChange Called when the checkbox is checked or unchecked.
+ *  Default implementation updates the task isDone property
+ */
 @Composable
-fun Task(task: TaskData, onCheckedChange: (Boolean) -> Unit){
+fun Task(
+    task: TaskData,
+    onTitleChange: (String) -> Unit = { task.title = it },
+    onDescriptionChange: (String) -> Unit = { task.description = it },
+    onCheckedChange: (Boolean) -> Unit = { task.isDone = it }
+){
     Row {
         Checkbox(checked = task.isDone, onCheckedChange = onCheckedChange)
         Column {
-            Text(text = task.title)
-            Text(text = task.description)
+            TextField(value = task.title, onValueChange = onTitleChange)
+            TextField(value = task.description, onValueChange = onDescriptionChange)
         }
     }
 }
